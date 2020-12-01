f9df1dcadbbc0f5aeb23cf20cb7ce3115141cfae
Caio Carvalho <caiocarvalho.py@gmail.com>
2020-12-01 17:26:52 -0400

    Renomeando os testes

--- src/leilao/test_avaliador.py
+++ src/leilao/test_avaliador.py
@@ -4,7 +4,7 @@ from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 
 class TestAvaliador(TestCase):
-    def test_avalia(self):
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
         gui = Usuario('Gui')
         yuri = Usuario('Yuri')
 
@@ -15,6 +15,27 @@ class TestAvaliador(TestCase):
 
         leilao.lances.append(lance_do_yuri)
         leilao.lances.append(lance_do_gui)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        lance_do_yuri = Lance(yuri, 100)
+        lance_do_gui = Lance(gui, 150)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_yuri)
 
         avaliador = Avaliador()
         avaliador.avalia(leilao)
