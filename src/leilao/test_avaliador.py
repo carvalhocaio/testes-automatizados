d64f5b30162706462aaa292653546fa740ae66aa
carvalhocaio <caiocarvalho.py@gmail.com>
2021-01-01 23:19:45 -0400

    '

--- src/leilao/test_avaliador.py
+++ src/leilao/test_avaliador.py
@@ -1,5 +1,4 @@
 from unittest import TestCase
-
 from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 
@@ -14,8 +13,8 @@ class TestAvaliador(TestCase):
         yuri = Usuario('Yuri')
         lance_do_yuri = Lance(yuri, 100.0)
 
-        self.leilao.lances.append(lance_do_yuri)
-        self.leilao.lances.append(self.lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
+        self.leilao.propoe(self.lance_do_gui)
 
         avaliador = Avaliador()
         avaliador.avalia(self.leilao)
@@ -30,8 +29,8 @@ class TestAvaliador(TestCase):
         yuri = Usuario('Yuri')
         lance_do_yuri = Lance(yuri, 100.0)
 
-        self.leilao.lances.append(self.lance_do_gui)
-        self.leilao.lances.append(lance_do_yuri)
+        self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
 
         avaliador = Avaliador()
         avaliador.avalia(self.leilao)
@@ -43,7 +42,7 @@ class TestAvaliador(TestCase):
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
-        self.leilao.lances.append(self.lance_do_gui)
+        self.leilao.propoe(self.lance_do_gui)
 
         avaliador = Avaliador()
         avaliador.avalia(self.leilao)
@@ -59,9 +58,9 @@ class TestAvaliador(TestCase):
 
         leilao = Leilao('Celular')
 
-        leilao.lances.append(self.lance_do_gui)
-        leilao.lances.append(lance_do_yuri)
-        leilao.lances.append((lance_do_vini))
+        leilao.propoe(self.lance_do_gui)
+        leilao.propoe(lance_do_yuri)
+        leilao.propoe(lance_do_vini)
 
         avaliador = Avaliador()
         avaliador.avalia(leilao)
